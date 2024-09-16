5d5f115f4ebce8f2d2a5d442e7ed6aee66a1b1b2
XR-369 <leddashi@gmail.com>
2024-09-16 13:24:35 +0000

    Copied the original code

--- scanner.py
+++ scanner.py
@@ -9,29 +9,28 @@ def enum(url):
     return url
 
 def brute(base_url, wordlist):
-    base_url =enum(base_url)
+    base_url = enum(base_url)  # Fixed function name 
     if not base_url.endswith('/'):
         base_url += '/'
-    
+
     for word in wordlist:
-        url = base_url + word.strip()
-        try:
-            response = response.get(url, timeout=1)
+        url = base_url + word.strip()  # fixed typo
+        try: 
+            response = requests.get(url, timeout=1)
 
             if response.status_code == 200:
                 print(f"found {url}")
             else:
                 print(f"Not found {url} {response.status_code}")
-            
-        except requests.exceptions.Timeout:
+
+        except requests.exceptions.Timeout:  # fixed typo
             print(f"Timeout with {url}")
-        except requests.ConnectionError:
-            print(f"Failed connection {url}")
+        except requests.ConnectionError:  # fixed typo
+            print(f"Failed Connection {url}")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("base_url")
-args = parser.parse_args
+parser.add_argument("base_url")  # Fixed typo: changed 'add_arguemnt' to 'add_argument'
+args = parser.parse_args()
 
-wordlist = [line for line in sys.stdin]
-brute(args.base_url, wordlist)
-
+wordlist = [line for line in sys.stdin]  
+brute(args.base_url, wordlist)  
